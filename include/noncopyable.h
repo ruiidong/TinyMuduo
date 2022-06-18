c9baacbdd1ccd66179da722e73b0563d5c0cdbf4
tencent-lrd <1337587618@qq.com>
2022-06-19 00:40:39 +0800

    class noncopyable

---
+++ include/noncopyable.h
@@ -0,0 +1,17 @@
+#pragma once
+
+/**
+ * 
+ * 继承该noncopyable类的子类无法调用拷贝构造函数和赋值函数
+ * 
+ */
+
+class noncopyable
+{
+public:
+    noncopyable(const noncopyable &) = delete;          //delete(禁用函数)
+    noncopyable &operator=(const noncopyable &) = delete;
+protected:
+    noncopyable() = default;                            //生成默认的函数定义提
+    ~noncopyable() = default;
+};
